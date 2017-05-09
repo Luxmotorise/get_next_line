cecc69baed2ac1605c0774a0d22ab3cb83afc7de
Robin DE DIEULEVEULT <rde-dieu@e2r2p9.42.fr>
2017-05-09 16:44:23 +0200

    WIP

--- get_next_line.c
+++ get_next_line.c
@@ -38,6 +38,7 @@ char	*ft_realloc(char *buff, int n)
 	t = malloc(n + 1);
 	t[0] = 0;
 	ft_strcat(t, buff);
+	//free(buff);
 	return (t);
 }
 
@@ -49,19 +50,27 @@ int		get_next_line(const int fd, char **line)
 	int				res;
 	int				n;
 
-	if (ft_strchr(lastbuff, '\n'))
-	{
-		*line = ft_realloc(*line, findeol(lastbuff));
-		*line = ft_strncpy(line, lastbuff, findeol(lastbuff));
-		lastbuff = ft_realloc(lastbuff, ft_strlen(lastbuff[eol]));
-		lastbuff = ft_strcpy(lastbuff, lastbuff[eol]);
-		return (1);
-	}
 	eol = -1;
+	//printf("toto\n");
+	printf("%s\n", lastbuff);
+	if (lastbuff)
+	{
+		if (ft_strchr(lastbuff, '\n'))
+		{
+			printf("toto\n");
+			*line = malloc(findeol(lastbuff));
+			ft_strncpy(*line, lastbuff, findeol(lastbuff));
+			ft_strcpy(lastbuff, lastbuff + eol);
+			return (1);
+		}
+	}
 	n = 0;
+	//printf("toto\n");
 	while (eol == -1)
 	{
-		buff = ft_realloc(buff, n * BUFF_SIZE);
+		//printf("toto\n");
+		buff = ft_realloc(buff, (n + 1) * BUFF_SIZE);
+		//printf("toto\n");
 		res = read(fd, buff + eol, BUFF_SIZE);
 		if (res == -1)
 		{
@@ -72,23 +81,26 @@ int		get_next_line(const int fd, char **line)
 			eol = 0;
 		else
 		{
-			// printf("toto\n");
-			// printf("%s\n", buff);
+			//printf("toto\n");
+			printf("%s\n", buff);
 			eol = findeol(buff);
-			// printf("%d\n", eol);
-			// printf("toto\n");
+			printf("%d\n", eol);
+			//printf("toto\n");
 			if (eol == -1)
 			{
 				n++;
 			}
 		}
-		eol = eol + (n * BUFF_SIZE);
 	}
-	// printf("%d\n", eol);
-	// printf("%zu", bufferlen);
-	*line = ft_realloc(*line, ft_strlen(lastbuff) + eol);
-	*line = lastbuff + ft_strncpy(line, buff, eol);
-	lastbuff = ft_realloc(lastbuff, ft_strlen(buff[eol]));
-	lastbuff = ft_strcpy(lastbuff, buff[eol]);
+	lastbuff = ft_strdup(buff + (eol + 1));
+	*line = malloc(ft_strlen(lastbuff) + eol + 1);
+	//printf("toto\n");
+	ft_strcpy(*line, lastbuff);
+	//printf("toto\n");
+	ft_strlcat(*line, buff, eol);
+	eol = eol + (n * BUFF_SIZE);
+	printf("%d\n", eol);
+	free(buff);
+	printf("%s\n", lastbuff);
 	return (1);
 }
